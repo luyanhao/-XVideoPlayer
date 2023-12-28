fd66c62550cd8bc188ba806f5b2777617079cc55
luyanhao <luyanhao@xiaomi.com>
2023-12-28 15:28:03 +0800

    添加播放进度条控件

--- app/src/main/java/com/lyhao/xvideoplayer/ui/NativeMediaPlayerActivity.java
+++ app/src/main/java/com/lyhao/xvideoplayer/ui/NativeMediaPlayerActivity.java
@@ -1,5 +1,6 @@
 package com.lyhao.xvideoplayer.ui;
 
+import static com.lyhao.xvideoplayer.media.FFMediaPlayer.MEDIA_PARAM_VIDEO_DURATION;
 import static com.lyhao.xvideoplayer.media.FFMediaPlayer.MEDIA_PARAM_VIDEO_HEIGHT;
 import static com.lyhao.xvideoplayer.media.FFMediaPlayer.MEDIA_PARAM_VIDEO_WIDTH;
 import static com.lyhao.xvideoplayer.media.FFMediaPlayer.MSG_DECODER_DONE;
@@ -13,11 +14,13 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.lyhao.xvideoplayer.R;
@@ -30,6 +33,8 @@ import java.util.Locale;
 public class NativeMediaPlayerActivity extends AppCompatActivity implements SurfaceHolder.Callback, FFMediaPlayer.EventCallback {
     public static final String TAG = "NativeMediaPlayerAct";
     private MySurfaceView mSurfaceView;
+    private SeekBar seek_bar;
+    private boolean mIsTouch;
     private FFMediaPlayer ffMediaPlayer;
     private static final int PERMISSION_REQUEST_CODE = 1;
     private String mVideoPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/byteflow/oceans.mp4";
@@ -39,6 +44,24 @@ public class NativeMediaPlayerActivity extends AppCompatActivity implements Surf
         setContentView(R.layout.activity_native_media_player);
         mSurfaceView = findViewById(R.id.surface_view);
         mSurfaceView.getHolder().addCallback(this);
+
+        seek_bar = findViewById(R.id.seek_bar);
+        seek_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                mIsTouch = true;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mIsTouch = false;
+            }
+        });
     }
 
     @Override
@@ -77,7 +100,9 @@ public class NativeMediaPlayerActivity extends AppCompatActivity implements Surf
                     break;
                 }
                 case MSG_DECODING_TIME: {
-                    LogUtil.d(TAG, "decoding --- to --- " + msgValue);
+                    if (!mIsTouch) {
+                        seek_bar.setProgress((int) msgValue);
+                    }
                     break;
                 }
             }
@@ -91,5 +116,11 @@ public class NativeMediaPlayerActivity extends AppCompatActivity implements Surf
         if (width * height != 0) {
             mSurfaceView.setAspectRatio(width, height);
         }
+
+        int duration = (int) ffMediaPlayer.getMediaParams(MEDIA_PARAM_VIDEO_DURATION);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            seek_bar.setMin(0);
+        }
+        seek_bar.setMax(duration);
     }
 }
